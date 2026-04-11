eaaf3a20ece83a7975a3780392869b8604273a60
lxvelyalina <imamalialina@gmail.com>
2026-04-11 14:39:33 -0400

    Update User.cs

--- PontelloApp/Models/User.cs
+++ PontelloApp/Models/User.cs
@@ -21,7 +21,6 @@ namespace PontelloApp.Models
         public string? AddressLine2 { get; set; }
         public string? City { get; set; }
         public string? Province { get; set; }
-        //public string? State { get; set; }
         public string? PostalCode { get; set; }
         public string? Country { get; set; }
 
